54b56122b2e771ba5aa6b1da92b8221078b4f117
Nikita <nikita.marcius@gmail.com>
2019-09-12 14:24:45 +0300

    separate folder api

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-import api from './index'
+import api from './api'
 
 // api.rest.tickers('btcusdt'.toUpperCase()).then(cb => console.log('tickers', cb))
 // api.stream.trades('btcusdt'.toUpperCase(), cb => console.log('trades', cb))
